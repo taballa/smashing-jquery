c278eba7d8bf8c2841747698d05895299f6a1bdd
taballa.hp-PD <taballa@gmail.com>
2013-06-28 12:09:43 +0800

    Added jquery-filter.php

---
+++ app/jquery-filter.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>jQuery filter</title>
+    <link rel="stylesheet" href="stylesheets/screen.css">
+</head>
+<body>
+    <h3>test livereload 156</h3>
+    <div id="container">
+    
+    </div>
+    <script src="scripts/vendor/requirejs/require.js"></script>
+    <script>
+    require(['scripts/common'], function(){
+        require(['filter'])
+    })
+    </script>
+</body>
+</html>
