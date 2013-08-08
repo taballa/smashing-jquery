1f12c4457944139858873aacef20c3ce0f6a6ea3
taballa.hp-PD <taballa@gmail.com>
2013-08-08 16:48:02 +0800

    Add a page open_in_new_window.php

---
+++ app/open_in_new_window.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>Open in new window</title>
+</head>
+<body>
+    <h3>Open in new window</h3>
+    <a href="index.html">index.html</a>
+</body>
+</html>
