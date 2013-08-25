2f18badea3cb1324155ab0fa342ce3ac69bc4ff4
taballa.hp-PD <taballa@gmail.com>
2013-08-25 21:23:20 +0800

    composing tabs_page.php

--- app/tabs_page.php
+++ app/tabs_page.php
@@ -6,39 +6,46 @@
     <?php require "header.php" ?>
 </head>
 <body class="tabs">
-    <h1>Tabs Page</h1>
-    <ul id="tabs" class="clearfix">
-        <li><a href="">Home</a></li>
-        <li><a href="">Shop</a></li>
-        <li><a href="">Community</a></li>
-        <li><a href="">Customer Service</a></li>
-        <li><a href="">About</a></li>
-    </ul>
-    <div id="content-container">
-        <div class="content">
-            <p>Compass Clearfix | Compass Documentation
-Compass Clearfix. A clearfix will extend the bottom of the element to enclose any floated elements it contains. This file can be imported using: @import ...
-compass-style.org/reference/compass/utilities/general/clearfix/ - 网页快照 - 类似结果</p>
-        </div>
-        <div class="content">
-            <p>Compass mixins you should know about | Steve McKinney
-3 Jan 2013 ... Compass is a brilliant extension to Sass as we all know. We know the time ... pie- clearfix. The simplest way to clearfix any element that needs it.
-iamsteve.me/blog/.../compass_mixins_you_should_know_about - 网页快照 - 类似结果</p>
-        </div>
-        <div class="content">
-            <p>SASS / SCSS Mixin for Clearfix - best approach? - Stack Overflow
-Why not use the Compass framework? It already provides mixins for clearfix among a host of other useful mixins and utilities. It's always better ...
-stackoverflow.com/.../sass-scss-mixin-for-clearfix-best-approach - 网页快照 - 类似结果</p>
-        </div>
-        <div class="content">
-            <p>What exactly does Compass / Blurprint's +clearfix do? - Stack Overflow
-Ususally, I use a div to clear the floats <div style="clear:both"></div> ... I'm running v0.10.5 and the source code partial in ... My guess is, it does ...
-stackoverflow.com/.../what-exactly-does-compass-blurprints-clearfix-do - 网页快照 - 类似结果</p>
-        </div>
-        <div class="content">
-            <p>Compass Will Change Your Life • 55 Minutes Blog
-24 Oct 2011 ... In which I aggressively push the adoption of Sass and Compass by every web ... { /*clearfix styles here*/ } ...
-blog.55minutes.com/2011/10/compass-will-change-your-life/ - 网页快照 - 类似结果</p>
+    <div class="container">
+        <h1>Tabs Page</h1>
+        <ul id="tabs" class="clearfix">
+            <li><a href="">Home</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">Community</a></li>
+            <li><a href="">Customer Service</a></li>
+            <li><a href="">About</a></li>
+        </ul>
+        <div id="content-container">
+            <div class="content">
+                <p>Compass Clearfix | Compass Documentation
+                    Compass Clearfix. A clearfix will extend the bottom of the element to enclose any floated elements it contains. This file can be imported using: @import ...
+                    compass-style.org/reference/compass/utilities/general/clearfix/ - 网页快照 - 类似结果
+                </p>
+            </div>
+            <div class="content">
+                <p>Compass mixins you should know about | Steve McKinney
+                    3 Jan 2013 ... Compass is a brilliant extension to Sass as we all know. We know the time ... pie- clearfix. The simplest way to clearfix any element that needs it.
+                    iamsteve.me/blog/.../compass_mixins_you_should_know_about - 网页快照 - 类似结果
+                </p>
+            </div>
+            <div class="content">
+                <p>SASS / SCSS Mixin for Clearfix - best approach? - Stack Overflow
+                    Why not use the Compass framework? It already provides mixins for clearfix among a host of other useful mixins and utilities. It's always better ...
+                    stackoverflow.com/.../sass-scss-mixin-for-clearfix-best-approach - 网页快照 - 类似结果
+                </p>
+            </div>
+            <div class="content">
+                <p>What exactly does Compass / Blurprint's +clearfix do? - Stack Overflow
+                    Ususally, I use a div to clear the floats ... I'm running v0.10.5 and the source code partial in ... My guess is, it does ...
+                    stackoverflow.com/.../what-exactly-does-compass-blurprints-clearfix-do - 网页快照 - 类似结果
+                </p>
+            </div>
+            <div class="content">
+                <p>Compass Will Change Your Life • 55 Minutes Blog
+                    24 Oct 2011 ... In which I aggressively push the adoption of Sass and Compass by every web ... { /*clearfix styles here*/ } ...
+                    blog.55minutes.com/2011/10/compass-will-change-your-life/ - 网页快照 - 类似结果
+                </p>
+            </div>
         </div>
     </div>
     <?php require "footer.php" ?>
