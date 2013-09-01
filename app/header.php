ac6fa3b6dd6aa70959d75b1c165819c71f8b83e7
taballa.hp-PD <taballa@gmail.com>
2013-09-01 10:29:56 +0800

    update header.php

--- app/header.php
+++ app/header.php
@@ -1,3 +1,13 @@
 <!-- build:css styles/screen.css -->
     <link rel="stylesheet" href="stylesheets/screen.css">
 <!-- endbuild -->
+
+<!-- build:css styles/ie.css -->
+<!--[if IE]>
+    <link href="stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<![endif]-->
+<!-- endbuild -->
+
+<!-- build:css styles/print.css -->
+<link href="stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+<!-- endbuild -->
