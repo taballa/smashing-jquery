252eb12c0db452cdf02153c39c8855d8245bb181
taballa.hp-PD <taballa@gmail.com>
2013-10-14 13:25:48 +0800

    update app.js

--- app/app.js
+++ app/app.js
@@ -1,36 +1,25 @@
 var express = require('express');
-
 var http = require('http');
-
 var app = express();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
 
 app.all("*", function(req, res, next){
-    // res.writeHead(404, {"Content-Type": "text/plain"});
+    // res.setHeader(404, {"Content-Type": "text/plain"});
     next();
 })
-
 app.get("/", function(req, res){
-    res.render("index", {message: "I love anime."});
-    // res.end("Welcome to the homepage.")
+    res.render("index", {message: "I love movie."});
 })
-
 app.get("/about", function(req, res){
     res.end("Welcome to the about page.")
 })
-
 app.get("*", function(req, res){
     res.end("404!")
 })
 
-http.createServer(app).listen(1222);
+http.createServer(app).listen(3000);
+console.log("Server running at http://localhost:3000")
 
-// var app = express();
-// app.get('/', function(req, res){
-//     res.send('hello world.');
-// });
-
-// app.listen(3000);
 
